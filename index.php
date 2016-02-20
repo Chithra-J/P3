5adb5cbc124d3e5f5f51ff6095c09d860aeaa7ec
Chithra-J <chithrajayakumar1@gmail.com>
2016-02-19 20:10:29 -0500

    Initial version

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<!-- Set the viewport so this responsive site displays correctly on mobile devices -->
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Chithra Jayakumar P3 Demo">
+		<meta name="author" content="Chithra Jayakumar">
+		<title>Dynamic Web Applications - Project P3</title>
+		<link href='css/p2.css' rel='stylesheet'>
+	</head>
+	<body id="bg">
+		<div class="layout">
+			<div class="course">
+				<h1>Dynamic Web Applications - Project P3</h1>
+				<h2>TBD</h2>
+				<h3>Chithra Jayakumar</h3>
+			</div>
+
+			<div id="projects">
+				<div class="project">
+					<a href="https://github.com/Chithra-J/P4" target="_blank">Project P4</a>
+				</div>
+				<div class="project">
+					<a href="https://github.com/Chithra-J/P3" target="_blank">Project P3</a>
+				</div>
+				<div class="project">
+					<a href="https://github.com/Chithra-J/P2" target="_blank">Project P2</a>
+				</div>
+				<div class="project">
+					<a href="http://p1.chanchika.me/" target="_blank">Project P1</a>
+				</div>
+			</div>
+			<div class="intro">
+				<br>
+				<br>
+				<p>
+					<br>
+				</p>
+				<p>
+					<br>
+				</p>
+				<p>
+					<b><u>About this project:</u></b>
+					<br>
+					<br>
+					Project P3 is to give hands-on practice with working with the essentials of building a Laravel-based site. Building this project will demonstrate an understanding of the following skills:
+
+					<ol>
+						<li>
+							Creating a new Laravel application.
+						</li>
+						<li>
+							Installing and using Composer packages.
+						</li>
+						<li>
+							Routing basics.
+						</li>
+						<li>
+							Views.
+						</li>
+						<li>
+							Deploying a Laravel app on a live server.
+						</li>
+					</ol>
+					<br>
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
