4aa3a91b94686e5cf1a3563fc6dda1121d28becf
Chithra-J <chithrajayakumar1@gmail.com>
2016-03-30 15:47:09 -0400

    Removed index.php file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,33 +1,28 @@
 <?php
 
-	Route::get('/', 'P3LoremIpsumController@getIndex');
-		
-	Route::get('/P3', 'P3LoremIpsumController@getIndex');
+/*
+ |--------------------------------------------------------------------------
+ | Application Routes
+ |--------------------------------------------------------------------------
+ |
+ | Here is where you can register all of the routes for an application.
+ | It's a breeze. Simply tell Laravel the URIs it should respond to
+ | and give it the controller to call when that URI is requested.
+ |
+ */
 
-	Route::get('/P3/lorem-ipsum', 'P3LoremIpsumController@getLoremIpsum');
-	
-	Route::post('/P3/lorem-ipsum', 'P3LoremIpsumController@postLoremIpsum');
-
-	Route::get('/P3/user-generator', 'P3RandomUserController@getRandomUsers');
-	
-	Route::post('/P3/user-generator', 'P3RandomUserController@postRandomUsers');
-	
-Route::group(['middleware' => ['web']], function() {
-
-	/*
-	Route::get('/', function() {
-			return view('welcome');
-		});
-		
-		Route::get('/P3', 'P3LoremIpsumController@getIndex');
-	
-		Route::get('/P3/lorem-ipsum', 'P3LoremIpsumController@getLoremIpsum');
-		
-		Route::post('/P3/lorem-ipsum', 'P3LoremIpsumController@postLoremIpsum');
-	
-		Route::get('/P3/user-generator', 'P3RandomUserController@getRandomUsers');
-		
-		Route::post('/P3/user-generator', 'P3RandomUserController@postRandomUsers');*/
-	
-
+Route::get('/', function() {
+	return redirect('/P3');
 });
+
+Route::get('/', 'P3LoremIpsumController@getIndex');
+
+Route::get('/P3', 'P3LoremIpsumController@getIndex');
+
+Route::get('/P3/lorem-ipsum', 'P3LoremIpsumController@getLoremIpsum');
+
+Route::post('/P3/lorem-ipsum', 'P3LoremIpsumController@postLoremIpsum');
+
+Route::get('/P3/user-generator', 'P3RandomUserController@getRandomUsers');
+
+Route::post('/P3/user-generator', 'P3RandomUserController@postRandomUsers');
