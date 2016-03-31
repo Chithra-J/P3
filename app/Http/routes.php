dbd7250ae794463bee83d539e4ca72c4fdf4c4d3
Chithra-J <chithrajayakumar1@gmail.com>
2016-03-30 20:17:59 -0400

    updated index.php file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,37 +1,34 @@
 <?php
 
-	/*
-	Route::get('/', 'P3LoremIpsumController@getIndex');
-			
-		Route::get('/P3', 'P3LoremIpsumController@getIndex');
-	
-		Route::get('/P3/lorem-ipsum', 'P3LoremIpsumController@getLoremIpsum');
-		
-		Route::post('/P3/lorem-ipsum', 'P3LoremIpsumController@postLoremIpsum');
-	
-		Route::get('/P3/user-generator', 'P3RandomUserController@getRandomUsers');
-		
-		Route::post('/P3/user-generator', 'P3RandomUserController@postRandomUsers');*/
-	
-	
-Route::group(['middleware' => ['web']], function() {
+Route::get('/', 'P3LoremIpsumController@getIndex');
+
+Route::get('/P3', 'P3LoremIpsumController@getIndex');
+
+Route::get('/P3/lorem-ipsum', 'P3LoremIpsumController@getLoremIpsum');
+
+Route::post('/P3/lorem-ipsum', 'P3LoremIpsumController@postLoremIpsum');
+
+Route::get('/P3/user-generator', 'P3RandomUserController@getRandomUsers');
+
+Route::post('/P3/user-generator', 'P3RandomUserController@postRandomUsers');
+
+/*Route::group(['middleware' => ['web']], function() {
 
 	
-	/*
-	Route::get('/', function() {
-				return view('welcome');
-			});*/
-	
-		Route::get('/', 'P3LoremIpsumController@getIndex');
-		Route::get('/P3', 'P3LoremIpsumController@getIndex');
-	
-		Route::get('/P3/lorem-ipsum', 'P3LoremIpsumController@getLoremIpsum');
-		
-		Route::post('/P3/lorem-ipsum', 'P3LoremIpsumController@postLoremIpsum');
-	
-		Route::get('/P3/user-generator', 'P3RandomUserController@getRandomUsers');
-		
-		Route::post('/P3/user-generator', 'P3RandomUserController@postRandomUsers');
-	
+	 Route::get('/', function() {
+	 return view('welcome');
+	 });*/
 
-});
+	/*
+	 Route::get('/', 'P3LoremIpsumController@getIndex');
+	 Route::get('/P3', 'P3LoremIpsumController@getIndex');
+
+	 Route::get('/P3/lorem-ipsum', 'P3LoremIpsumController@getLoremIpsum');
+
+	 Route::post('/P3/lorem-ipsum', 'P3LoremIpsumController@postLoremIpsum');
+
+	 Route::get('/P3/user-generator', 'P3RandomUserController@getRandomUsers');
+
+	 Route::post('/P3/user-generator', 'P3RandomUserController@postRandomUsers');
+
+});*/
